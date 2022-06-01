8c7d556894cac5c9cb23b96dd356b700a80dd844
Matthew DeMichele <m.d.demichele@gmail.com>
2022-06-01 08:43:43 -0700

    finished working version of project

--- client.py
+++ client.py
@@ -2,18 +2,41 @@
 import socket 
 
 HOST = "localhost"
-PORT = 10023
+PORT = 10892
 
 # Create a new socket object for use
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # Connect to specified host and port 
     s.connect((HOST, PORT))
     
-    # Send GET request 
-    s.sendall(b'Are we connected?')
-    print("Message Sent")
-    # Receive the message back from the server 
-    data = s.recv(1024)
+    continueConversation = True 
+    print(f'Connected to {HOST} at port {PORT}.')
+    print("Type \q to quit.")
+    while continueConversation:
     
-# Print the resulting message from the server in the terminal 
-print(data.decode('ascii'))
+        # Prompt user for input 
+        message = input(">>>> ")
+        
+        # Limit messages to 1024 bytes length 
+        while len(message) > 900:
+            print("\n")
+            print("ERROR! Your message is too long. Type in something shorter please.")
+            message = input(">>>> ")
+        
+        # close socket if \q is typed 
+        if message == "\q":
+            message = "The Client has left the conversation. Type \q to quit."
+            byteMessage = bytes(message, 'ascii')
+            s.sendall(byteMessage)
+            continueConversation = False 
+            s.close()
+            
+        # Send message to server and receive response back 
+        else:
+            byteMessage = bytes(message, 'ascii')
+            s.sendall(byteMessage)
+        
+            # Receive response back from the server 
+            response = s.recv(1024)
+            print("SERVER: {}".format(response.decode('ascii')))
+        
--- server.py
+++ server.py
@@ -3,26 +3,45 @@ import socket
 
 # Declare variables for the host name and port number 
 HOST = "localhost"
-PORT = 10023
+PORT = 10892
 
 # Create a listening socket 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     
     # Bind socket to host and port 
     s.bind((HOST, PORT))
+    print("Listening for connections on port {}...".format(PORT))
+    print("Type \q to quit.")
+    # Listen for one connection at a time 
+    s.listen(1)
+        
+    # Accept a connection (conn is the new socket object)
+    conn, addr = s.accept()
     
-    # Let the server listen for new connections indefinitely
-    while True:
-        # Listen for one connection at a time 
-        s.listen(1)
-        print("Listening for connections on port {}...".format(PORT))
-        # Accept a connection (conn is the new socket object)
-        conn, addr = s.accept()
-        
-        # Read the socket request 
+    continueConversation = True 
+
+    while continueConversation:
+        # Read the message from the socket 
         data = conn.recv(1024)
-        print("RECIEVED <<<<<<<<<")
-        print(data.decode('ascii'))
+        print("CLIENT: {}".format(data.decode('ascii')))
         
-        # Close conn socket object 
-        conn.close()
+        # Prompt response from server 
+        response = input(">>>> ")
+        
+        # Check that the message is less than 1024 bytes 
+        while len(response) > 900:
+            print("\n")
+            print("ERROR! Your message is too long. Type in something shorter please.")
+            response = input(">>>> ")
+        
+        # Close conn socket object and exit loop
+        if response == "\q":
+            byteResponse = bytes("Server has left the conversation...Quit by typing \q.", 'ascii')
+            conn.sendall(byteResponse)
+            continueConversation = False  
+            conn.close()
+        # Send response back to client
+        else:
+            byteResponse = bytes(response, 'ascii')
+            conn.sendall(byteResponse)
+        
